eb7928a5661f2294174cb33f435668fa475f8644
Thomas Rothe <eingefuchst@gmail.com>
2016-07-22 11:39:34 +0800

    added comments

--- IndependentObserverPattern/subject/Subject.h
+++ IndependentObserverPattern/subject/Subject.h
@@ -15,19 +15,39 @@
 
 #include "../event/Event.h"
 
+/**
+ * Subject represents a generic and minimal Subject implementation.
+ * Subject dont need knowledge of a Observer implementation.
+ * We reduced the dependency and forced working with direct callbacks
+ * We can use a combinaiton of std::function and std::bind for anything,
+ * here for delegate any method, based on our argument list and return value.
+ *
+ * STATUS: unregister missing, thats tricky
+ */
+
+
 class Subject
 {
 public:
-	Subject()=default;
-	Subject(const Subject &)=delete;
-	Subject& operator=(const Subject &)=delete;
+	Subject()=default; 								/// use default ctor
+	Subject(const Subject &)=delete;				/// delete copy stor
+	Subject& operator=(const Subject &)=delete;		/// delete assign operator
 
+	/**
+	 * Method to register events and their callbacks
+	 * @param event_ keytype of expected event
+	 * @param observer_ anything what match with out std::function declaration
+	 */
 	template<typename Observer>
 	void registerObserver(const Event& event_, Observer&& observer_)
 	{
 		m_obsCol[event_].push_back(std::forward<Observer>(observer_));
 	}
 
+	/**
+	 * Notify registered subcriber
+	 * @param event_ keynode which event shall be notified
+	 */
 	void notify(const Event& event_)
 	{
 		for(const auto & obs : m_obsCol[event_])
@@ -35,7 +55,7 @@ public:
 	}
 
 private:
-	std::map<Event, std::vector<std::function<void()>>> m_obsCol;
+	std::map<Event, std::vector<std::function<void()>>> m_obsCol;		/// collection to store keynodes and all registered events
 };
 
 
