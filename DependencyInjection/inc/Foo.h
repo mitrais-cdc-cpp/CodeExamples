5915424f733b0b2e05a55f817e94b86ef0509e57
suhendra-ep <IKomangSuhendra.EkaPutra@mitrais.com>
2016-07-29 13:46:36 +0800

    Create the skeleton project for Dependency Injection in C++

---
+++ DependencyInjection/inc/Foo.h
@@ -0,0 +1,15 @@
+/*
+ * Foo.h
+ *
+ *  Created on: Jul 29, 2016
+ *      Author: developer
+ */
+
+#ifndef INC_FOO_H_
+#define INC_FOO_H_
+
+
+
+
+
+#endif /* INC_FOO_H_ */
---
+++ DependencyInjection/main.cpp
@@ -0,0 +1,18 @@
+/*============================================================================
+* Name        : Dependency Injection in C++
+* Author      : I Komang Suhendra Eka Putra
+* Version     :
+* Copyright   :
+* Description : Implementation of Dependency Injection in C++.
+* This project created for C++ Bootcamp Training in Mitrais.
+==============================================================================*/
+
+#include <iostream>
+using namespace Mitrais;
+
+
+int main(int argc, char* argv[])
+{
+	return 0;
+}
+
