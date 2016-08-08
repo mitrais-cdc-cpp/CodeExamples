ebf622f5392f9d265fa8c4c5ac7bcd286ed7e89d
albertusadityo <didithere@gmail.com>
2016-08-08 09:38:22 +0700

    added comment lines for explanation how it works

--- Memento/main.cpp
+++ Memento/main.cpp
@@ -15,24 +15,24 @@ using namespace std;
 int main() {
 	std::string val = "begin";
 
-	Text _txt(val);
-	cout << _txt.getText() <<std::endl;
+	Text _txt(val); // create Text class with initial string of val
+	cout << _txt.getText() <<std::endl; // begin
+	CareTaker::getInstance()->_textMemento = _txt.createMemento(); // save the initial string to a memento
 
 	/**
 	 * Save state
 	 */
-	CareTaker::getInstance()->_textMemento = _txt.createMemento();
-	val = "changed";
-	_txt.setText(val);
-	cout << _txt.getText() <<std::endl;
+	val = "changed"; // change the string of val
+	_txt.setText(val); // set the modified val to txt object
+	cout << _txt.getText() <<std::endl; // changed
 
 	/**
 	 * Restore state
 	 */
-	TextMemento txtMem = _txt.createMemento();
-	_txt.reinstateMemento(CareTaker::getInstance()->_textMemento);
-	CareTaker::getInstance()->_textMemento = txtMem;
-	cout << _txt.getText() << std::endl;
+	TextMemento txtMem = _txt.createMemento(); // store the modified txt state to a temp memento
+	_txt.reinstateMemento(CareTaker::getInstance()->_textMemento); // reinstate the txt to previous state
+	CareTaker::getInstance()->_textMemento = txtMem; // store the temp memento to a caretaker
+	cout << _txt.getText() << std::endl; // begin
 
 	return 0;
 }
